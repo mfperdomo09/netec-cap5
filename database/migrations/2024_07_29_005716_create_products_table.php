095511fefbb1e9341de0fc7f96bd5fa524d0c951
Jhoan Montealegre <jhoanmontealegre92@gmail.com>
2024-07-29 13:20:22 -0500

    Update 2024_07_29_005716_create_products_table.php

--- database/migrations/2024_07_29_005716_create_products_table.php
+++ database/migrations/2024_07_29_005716_create_products_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
 
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
 
+            $table->softDeletes();
             $table->timestamps();
         });
     }
